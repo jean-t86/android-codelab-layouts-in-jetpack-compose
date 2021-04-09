724e149fd2fba58dbb9164699fab246f72ef2d45
J.Tadebois <jpm.tadebois@gmail.com>
2021-04-09 18:17:42 +0400

    Add completed code for Modifiers section

--- app/src/main/java/au/com/broscience/layoutsinjetpackcomposecodelab/MainActivity.kt
+++ app/src/main/java/au/com/broscience/layoutsinjetpackcomposecodelab/MainActivity.kt
@@ -3,14 +3,20 @@ package au.com.broscience.layoutsinjetpackcomposecodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,17 +50,29 @@ fun DefaultPreview() {
 }
 
 @Composable
-fun PhotographerCard() {
-    Row {
+fun PhotographerCard(modifier: Modifier = Modifier) {
+    Row(
+        modifier
+            .clip(RoundedCornerShape(4.dp))
+            .background(MaterialTheme.colors.surface)
+            .clickable(onClick = { })
+            .padding(16.dp)
+    ) {
         Surface(
-            modifier = Modifier.size(50.dp),
+            modifier = Modifier
+                .size(50.dp)
+                .align(CenterVertically),
             shape = CircleShape,
             color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
         ) {
             // Images goes here
         }
 
-        Column {
+        Column(
+            modifier = Modifier
+                .align(CenterVertically)
+                .padding(start = 8.dp)
+        ) {
             Text("Alfred Sisley", fontWeight = FontWeight.Bold)
             CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
                 Text("3 minutes ago", style = MaterialTheme.typography.body2)
