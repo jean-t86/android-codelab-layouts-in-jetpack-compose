febb546936b5e88d055f0849b54d1f9861b6db25
J.Tadebois <jpm.tadebois@gmail.com>
2021-04-09 18:00:38 +0400

    Add skeleton code for photographer card

--- app/src/main/java/au/com/broscience/layoutsinjetpackcomposecodelab/MainActivity.kt
+++ app/src/main/java/au/com/broscience/layoutsinjetpackcomposecodelab/MainActivity.kt
@@ -3,11 +3,17 @@ package au.com.broscience.layoutsinjetpackcomposecodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import au.com.broscience.layoutsinjetpackcomposecodelab.ui.theme.LayoutsinjetpackcomposecodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,5 +40,33 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     LayoutsinjetpackcomposecodelabTheme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun PhotographerCard() {
+    Row {
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+            // Images goes here
+        }
+
+        Column {
+            Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text("3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PhotographerCardPreview() {
+    LayoutsinjetpackcomposecodelabTheme {
+        PhotographerCard()
     }
 }
