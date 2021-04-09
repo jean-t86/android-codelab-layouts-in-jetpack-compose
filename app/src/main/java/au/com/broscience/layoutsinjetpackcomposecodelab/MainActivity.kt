72e277a48e6c24d0f193f9b905cd09b417d51c65
J.Tadebois <jpm.tadebois@gmail.com>
2021-04-09 23:01:35 +0400

    Add completed code for Working with lists

--- app/src/main/java/au/com/broscience/layoutsinjetpackcomposecodelab/MainActivity.kt
+++ app/src/main/java/au/com/broscience/layoutsinjetpackcomposecodelab/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
@@ -27,6 +28,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import au.com.broscience.layoutsinjetpackcomposecodelab.ui.theme.LayoutsinjetpackcomposecodelabTheme
 import com.google.accompanist.coil.CoilImage
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,7 +36,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             LayoutsinjetpackcomposecodelabTheme {
                 LayoutsCodelab { modifier ->
-                    LazyList(modifier)
+                    ScrollingList(modifier)
                 }
             }
         }
@@ -147,12 +149,39 @@ fun SimpleListPreview() {
 }
 
 @Composable
-fun LazyList(modifier: Modifier = Modifier) {
+fun ScrollingList(modifier: Modifier = Modifier) {
+    val listSize = 100
+
+    // We save the scrolling position with this state
     val scrollState = rememberLazyListState()
 
-    LazyColumn(state = scrollState) {
-        items(100) { index ->
-            ImageListItem(index = index)
+    // We save the coroutine scope where our animated scroll will be executed
+    val coroutineScope = rememberCoroutineScope()
+    
+    Column {
+        Row {
+            Button(onClick = {
+                coroutineScope.launch {
+                    // 0 is the first item index
+                    scrollState.animateScrollToItem(0)
+                }
+            }) {
+                Text(text = ("Scroll to the top"))
+            }
+
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(listSize - 1)
+                }
+            }) {
+                Text(text = ("Scroll to the end"))
+            }
+        }
+
+        LazyColumn(state = scrollState) {
+            items(listSize) { index ->
+                ImageListItem(index = index)
+            }
         }
     }
 }
@@ -162,7 +191,7 @@ fun LazyList(modifier: Modifier = Modifier) {
 fun PreviewLazyList() {
     LayoutsinjetpackcomposecodelabTheme {
         LayoutsCodelab {
-            LazyList(it)
+            ScrollingList(it)
         }
     }
 }
