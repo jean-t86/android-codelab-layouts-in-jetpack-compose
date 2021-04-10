2b309ecc9655ca9370359f4a0b836afa2ce2891f
J.Tadebois <jpm.tadebois@gmail.com>
2021-04-10 17:22:22 +0400

    Add custom modifier function to add padding from first baseline

--- app/src/main/java/au/com/broscience/layoutsinjetpackcomposecodelab/MainActivity.kt
+++ app/src/main/java/au/com/broscience/layoutsinjetpackcomposecodelab/MainActivity.kt
@@ -23,8 +23,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import au.com.broscience.layoutsinjetpackcomposecodelab.ui.theme.LayoutsinjetpackcomposecodelabTheme
 import com.google.accompanist.coil.CoilImage
@@ -157,7 +161,7 @@ fun ScrollingList(modifier: Modifier = Modifier) {
 
     // We save the coroutine scope where our animated scroll will be executed
     val coroutineScope = rememberCoroutineScope()
-    
+
     Column {
         Row {
             Button(onClick = {
@@ -206,5 +210,40 @@ fun ImageListItem(index: Int) {
         )
         Spacer(modifier = Modifier.width(10.dp))
         Text("Item #$index", style = MaterialTheme.typography.subtitle1)
+    }
+}
+
+fun Modifier.firstBaselineToTop(
+    firstBaselineToTop: Dp
+) = Modifier.layout { measurable, constraints ->
+    val placeable = measurable.measure(constraints = constraints)
+
+    // Check if the composable has a first baseline
+    check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+    val firstBaseline = placeable[FirstBaseline]
+
+    // Height of the composable with padding - first baseline
+    val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+    val height = placeable.height + placeableY
+
+    layout(placeable.width, height = height) {
+        // Where the composable gets placed
+        placeable.placeRelative(0, placeableY)
+    }
+}
+
+@Preview
+@Composable
+fun TextWithPaddingToBaselinePreview() {
+    LayoutsinjetpackcomposecodelabTheme() {
+        Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
+    }
+}
+
+@Preview
+@Composable
+fun TextWithNormalPaddingPreview() {
+    LayoutsinjetpackcomposecodelabTheme {
+        Text("Hi there!", Modifier.padding(top = 32.dp))
     }
 }
